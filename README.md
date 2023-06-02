42e5ac33876287b74a7faa4752dac8971c96deb3
narumincho <16481886+narumincho@users.noreply.github.com>
2023-06-02 08:17:36 +0000

    add dart

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # http-server-quine
 
 [![Open in Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io/#https://github.com/narumincho/http-server-quine)
+
+Quine は 自身のソースコードを出力するプログラムです.
+
+出力先を HTTPサーバーのレスポンスにしたのが, この http-server-quine です.
+
+# 実装言語
+
+| language name | command                         |
+| ------------- | ------------------------------- |
+| dart          | `dart run ./dart/bin/dart.dart` |
+
+# 参加方法
+
+どしどし参加してほしい
+
+1. このリポジトリをフォークする
+2. [.gitpod.yml](.gitpod.yml) に言語のインストールコマンドを追加する
+3. 言語の名前のディレクトリを直下に追加し, その言語で http-server-quine
+   を実装する
+4. Pull Request を作成する
+5. [narumincho](https://github.com/narumincho) が動作するか確認
+   調整しマージする.
+
+# ルール
+
+1. ポートは環境変数の PORT から読み取る. 読み取れなかった場合は 8080
+2. ファイルの読み取りはしない
---
+++ dart/bin/dart.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+Future<void> main() async {
+  final port = int.parse(Platform.environment['PORT'] ?? '8080');
+  final server = await HttpServer.bind(InternetAddress.anyIPv4, port);
+  final code = 'import \'dart:io\';\n\nFuture<void> main() async {\n  final port = int.parse(Platform.environment[\'PORT\'] ?? \'8080\');\n  final server = await HttpServer.bind(InternetAddress.anyIPv4, port);\n  final code = !!!;\n\n  print(\'Serving at http://\${server.address.host}:\${server.port}\');\n\n  server.listen((request) {\n    request.response.headers.contentType = ContentType.text;\n    request.response.write(code.replaceFirst(\n        \'!!!\',\n        \'\'\' +\n            code\n                .replaceAll(\'\\\\\', \'\\\\\\\')\n                .replaceAll(\'\\\\\'\', \'\\\\\'\')\n                .replaceAll(\'\\\$\', \'\\\\\\\$\')\n                .replaceAll(\'\\\\n\', \'\\\\\\n\') +\n            \'\'\'));\n    request.response.close();\n  });\n}\n\n';
+
+  print('Serving at http://${server.address.host}:${server.port}');
+
+  server.listen((request) {
+    request.response.headers.contentType = ContentType.text;
+    request.response.write(code.replaceFirst(
+        '!!!',
+        ''' +
+            code
+                .replaceAll('\\', '\\\')
+                .replaceAll('\\'', '\\'')
+                .replaceAll('\$', '\\\$')
+                .replaceAll('\\n', '\\\n') +
+            '''));
+    request.response.close();
+  });
+}
